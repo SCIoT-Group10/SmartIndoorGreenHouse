749294ac98af8870571104e228ffde1b8316c092
Björn Müller <bjoern.mueller@kanu-baerchen.de>
2020-07-09 00:03:19 +0200

    save Data for planing

--- data_consumer.py
+++ data_consumer.py
@@ -1,3 +1,5 @@
+import json
+
 import pika
 
 credentials = pika.PlainCredentials('user', 'password')
@@ -11,8 +13,19 @@ channel.queue_declare(queue='sciot.temperature')
 channel.queue_bind(queue='sciot.temperature', exchange='sciot.topic', routing_key='u38.0.353.*.temperature.*')
 
 
+def planning(data):
+    time = data['time']
+    temperature = data['temperature']
+    humidity = data['humidity']
+    lightLevel = data['lightLevel']
+    waterLevel = data['waterLevel']
+    pass
+
+
 def callback(ch, method, properties, body):
-    print('Received: {}'.format(body))
+    data = json.loads(body)
+    planning(data)
+    print('Received: {}'.format(data))
 
 
 channel.basic_consume(queue='sciot.temperature', on_message_callback=callback, auto_ack=True)
